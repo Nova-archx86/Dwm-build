97addc3b6f9124b87b3efbd3bbaac89e839311b5
Nova-archx86 <moorcode66@yahoo.com>
2022-07-11 20:20:49 -0700

    Added some keybinds and cleaned the directory

--- config.h
+++ config.h
@@ -19,7 +19,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { " ", " ", " ", " ", " ", "6", "7", "8", "9" };
+static const char *tags[] = { " ", " ", " ", " ", " ", " ", "7", "8", "9" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -59,12 +59,18 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
+static const char *screencmd[] = { "shotgun", NULL };
+static const char *web[] = { "librewolf", NULL };
+static const char *lock[] = { "slock", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
+    { MODKEY,                       XK_x,      spawn,          {.v = lock } },  
+    { MODKEY,                       XK_s,      spawn,          {.v = screencmd } }, 
+    { MODKEY,                       XK_w,      spawn,          {.v = web } },
+    { MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
+    { MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
